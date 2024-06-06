3066d8a8048997afc5520294c3608d20cbab5536
Raphael Ochsenbein <raphael@akehir.com>
2024-06-06 15:45:08 +0200

    Migrate to angular@18

--- projects/example-app-standalone/src/app/app.component.ts
+++ projects/example-app-standalone/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ExampleComponent} from "./example.component";
 
